8270298f9f0df6bac1247cd7b968aa9e6cc62e82
tml27 <ugactye12@gmail.com>
2026-03-13 22:28:39 +0700

    Two Knights

---
+++ Introductory Problems/1072.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define all(dataStructure) dataStructure.begin(),dataStructure.end()
+
+using namespace std;
+
+typedef long long ll;
+
+const int MAX = 1e5 + 3;
+const int MOD[] = {1000000007, 998244353};
+
+void Solve() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        ll res = 1ll * i * i;
+        res = res * (res - 1) / 2;
+        res -= 4ll * (i - 1) * (i - 2);
+        cout << res << '\n';
+    }
+}
+
+int32_t main() {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0); std::cout.tie(0);
+
+    #define TASK ""
+    if (fopen(TASK".INP", "r")) {
+        freopen(TASK".INP", "r", stdin);
+        freopen(TASK".OUT", "w", stdout);
+    }
+
+    if (fopen("TASK.INP", "r")) {
+        freopen("TASK.INP", "r", stdin);
+        freopen("TASK.OUT", "w", stdout);
+    }
+
+    /* int TEST = 1; cin >> TEST; while (TEST--) */ Solve();
+
+    cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s\n";
+    return 0;
+}
