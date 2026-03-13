d7d1f5f681abb8a20c3e5a58454d84ff78f48811
tml27 <ugactye12@gmail.com>
2026-03-13 17:40:30 +0700

    Repetitions

---
+++ Introductory Problems/1069.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#define all(dataStructure) dataStructure.begin(),dataStructure.end()
+
+using namespace std;
+
+typedef long long ll;
+
+const int MAX = 1e5 + 3;
+const int MOD[] = {1000000007, 998244353};
+
+void Solve() {
+    string st;
+    cin >> st;
+    char last = '_';
+    int ans = 0, cnt = 0;
+    for (char &c : st) {
+        if (c == last) cnt++;
+        else {
+            ans = max(ans, cnt);
+            last = c;
+            cnt = 1;
+        }
+    }
+    ans = max(ans, cnt);
+    cout << ans;
+}
+
+int32_t main() {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0); std::cout.tie(0);
+
+    #define TASK ""
+    if (fopen(TASK".INP", "r")) {
+        freopen(TASK".INP", "r", stdin);
+        freopen(TASK".OUT", "w", stdout);
+    }
+
+    if (fopen("TASK.INP", "r")) {
+        freopen("TASK.INP", "r", stdin);
+        freopen("TASK.OUT", "w", stdout);
+    }
+
+    /* int TEST = 1; cin >> TEST; while (TEST--) */ Solve();
+
+    cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s\n";
+    return 0;
+}
