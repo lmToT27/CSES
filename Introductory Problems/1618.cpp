b3a054a6ea9ec3e94c7387b3cd1e8ff6d8b499ea
tml27 <ugactye12@gmail.com>
2026-03-14 12:59:38 +0700

    Trailing Zeros

---
+++ Introductory Problems/1618.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define all(dataStructure) dataStructure.begin(),dataStructure.end()
+
+using namespace std;
+
+typedef long long ll;
+
+const int MAX = 1e5 + 3;
+const int MOD[] = {1000000007, 998244353};
+
+void Solve() {
+    ll n;
+    cin >> n;
+    ll f = 0, g = 0;
+    ll a = 2, b = 5;
+    while (a <= n) {
+        f += n / a;
+        a *= 2;
+    }
+    while (b <= n) {
+        g += n / b;
+        b *= 5;
+    }
+    cout << min(f, g);
+}
+
+int32_t main() {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0); std::cout.tie(0);
+
+    #define TASK ""
+    if (fopen(TASK".INP", "r")) {
+        freopen(TASK".INP", "r", stdin);
+        freopen(TASK".OUT", "w", stdout);
+    }
+
+    if (fopen("TASK.INP", "r")) {
+        freopen("TASK.INP", "r", stdin);
+        freopen("TASK.OUT", "w", stdout);
+    }
+
+    /* int TEST = 1; cin >> TEST; while (TEST--) */ Solve();
+
+    cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s\n";
+    return 0;
+}
