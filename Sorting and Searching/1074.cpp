6d592f572eeb7c8bfad412d9cf5c6cd422982bc6
tml27 <ugactye12@gmail.com>
2026-03-23 14:44:38 +0700

    Stick Lengths

---
+++ Sorting and Searching/1074.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define all(dataStructure) dataStructure.begin(),dataStructure.end()
+
+using namespace std;
+
+typedef long long ll;
+
+const int MAX = 1e5 + 3;
+const int MOD[] = {1000000007, 998244353};
+
+void Solve() {
+    int n; cin >> n;
+    vector <int> a(n);
+    for (int &x : a) cin >> x;
+    sort(all(a));
+    ll prefix = 0, suffix = accumulate(all(a), 0ll);
+    ll ans = LLONG_MAX;
+    for (int i = 0; i < n; i++) {
+        suffix -= a[i];
+        ans = min(ans, 1ll * a[i] * i - prefix + suffix - 1ll * a[i] * (n - i - 1));
+        prefix += a[i];
+    }
+    cout << ans;
+}
+
+int32_t main() {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0); std::cout.tie(0);
+
+    #define TASK ""
+    if (fopen(TASK".INP", "r")) {
+        freopen(TASK".INP", "r", stdin);
+        freopen(TASK".OUT", "w", stdout);
+    }
+
+    if (fopen("TASK.INP", "r")) {
+        freopen("TASK.INP", "r", stdin);
+        freopen("TASK.OUT", "w", stdout);
+    }
+
+    /* int TEST = 1; cin >> TEST; while (TEST--) */ Solve();
+
+    cerr << "\nTime elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << "s\n";
+    return 0;
+}
